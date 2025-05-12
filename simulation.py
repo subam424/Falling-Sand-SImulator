b722594dead268b54eb4efd110be66fadbd59412
Subam Luitel <subams297@gmail.com>
2025-05-12 19:54:59 +0545

    added instructions

--- simulation.py
+++ simulation.py
@@ -9,6 +9,9 @@ HEIGHT = 700
 FPS = 120
 BG = (29, 29, 29)
 CELL_SIZE = 7
+FONT = pygame.font.SysFont("arial", 20)
+LINE_1 = FONT.render("C - Create [ S - Sand | R - Rock | W - Water ]", True, (255, 255, 255))
+LINE_2 = FONT.render("E - Eraser | SPACE - Reset", True, (255, 255, 255))
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Falling Sand")
@@ -21,8 +24,8 @@ while True:
 
     simulation.update()
 
-    # DRAW THE SCREEN
     simulation.draw(screen, BG)
-
+    screen.blit(LINE_1, (10, 10))
+    screen.blit(LINE_2, (10, 35))
     pygame.display.flip()
     clock.tick(FPS) 
